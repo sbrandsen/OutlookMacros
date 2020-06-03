143bf63d1cf484d429ffc1665994f70265e0c324
sbrandsen <45394133+sbrandsen@users.noreply.github.com>
2020-06-03 15:05:09 +0200

    Update removeduplicates.vb
    Now searches recursively

--- removeduplicates.vb
+++ removeduplicates.vb
@@ -1,7 +1,45 @@
 Option Explicit
+Dim deletedcount As Integer
 
-Const strPath = "c:\temp\deleted msg.csv"
-Sub DeleteDuplicateEmails()
+Private Sub Main()
+    Dim objNameSpace As Outlook.NameSpace
+    Dim objMainFolder As Outlook.Folder
+    Dim objDeletedFolder As Outlook.Folder
+    
+    Set objNameSpace = Application.GetNamespace("MAPI")
+    
+    MsgBox "Which folder should I search recusively?"
+    Set objMainFolder = objNameSpace.PickFolder
+    
+    MsgBox "Which folder should the duplicates be moved to?"
+    Set objDeletedFolder = objNameSpace.PickFolder
+    
+    If objMainFolder Is Nothing Then Exit Sub
+    If objDeletedFolder Is Nothing Then Exit Sub
+    
+    Call ProcessCurrentFolder(objMainFolder, objDeletedFolder)
+    MsgBox "Deleted " & deletedcount & " duplicates"
+    
+End Sub
+ 
+Private Sub ProcessCurrentFolder(ByVal objParentFolder As Outlook.MAPIFolder, objDeletedFolder As Folder)
+    Dim objCurFolder As Outlook.MAPIFolder
+    Dim objMail As Outlook.MailItem
+    
+    On Error Resume Next
+    
+    Call DeleteDuplicateEmails(objParentFolder, objDeletedFolder)
+    
+    '    Process the subfolders in the folder recursively
+    If (objParentFolder.Folders.Count > 0) Then
+        For Each objCurFolder In objParentFolder.Folders
+            Call ProcessCurrentFolder(objCurFolder, objDeletedFolder)
+        Next
+    End If
+End Sub
+
+
+Sub DeleteDuplicateEmails(olFolder As Folder, olFolder2 As Folder)
 
 Dim lngCnt As Long
 Dim objMail As Object
@@ -12,27 +50,10 @@ Dim objDic As Object
 Dim objItem As Object
 Dim olApp As Outlook.Application
 Dim olNS As NameSpace
-Dim olFolder As Folder
-Dim olFolder2 As Folder
 Dim strCheck As String
 
 Set objDic = CreateObject("scripting.dictionary")
 Set objFSO = CreateObject("scripting.filesystemobject")
-Set objTF = objFSO.CreateTextFile(strPath)
-objTF.WriteLine "Subject"
-
-Set olApp = Outlook.Application
-Set olNS = olApp.GetNamespace("MAPI")
-Set olFolder = olNS.PickFolder
-
-If olFolder Is Nothing Then Exit Sub
-
-On Error Resume Next
-Set olFolder2 = olFolder.Folders("removed items")
-On Error GoTo 0
-
-If olFolder2 Is Nothing Then Set olFolder2 = olFolder.Folders.Add("removed items")
-
 
 For lngCnt = olFolder.Items.Count To 1 Step -1
 
@@ -43,15 +64,9 @@ strCheck = Replace(strCheck, ", ", Chr(32))
 
     If objDic.Exists(strCheck) Then
        objItem.Move olFolder2
-       objTF.WriteLine Replace(objItem.Subject, ", ", Chr(32))
+       deletedcount = deletedcount + 1
     Else
         objDic.Add strCheck, True
     End If
 Next
-
-If objTF.Line > 2 Then
-    MsgBox "duplicate items were removed to ""removed items""", vbCritical, "See " & strPath & " for details"
-Else
-    MsgBox "No duplicates found"
-End If
 End Sub
